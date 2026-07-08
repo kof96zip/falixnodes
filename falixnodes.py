5948f43dcbe0b6c67e1dbba74133c9fe71310805
kof96zip <kof96zip@gmail.com>
2026-07-08 11:59:33 +0800

    Update falixnodes.py

--- falixnodes.py
+++ falixnodes.py
@@ -22,14 +22,11 @@ from seleniumbase import SB
 
 # ================= 配置区域 =================
 PROXY_URL = os.getenv("PROXY", "")  # 代理
-EMAIL = os.getenv("EMAIL")  # 邮箱
-PASSWORD = os.getenv("PASSWORD")  # 密码
 NUM = os.getenv("NUM")  # 服务器编号
 TG_TOKEN = os.getenv("TG_TOKEN")  # tg通知token
 TG_CHAT_ID = os.getenv("TG_CHAT_ID")  # tg通知chat_id
 
 # 目标 URL
-LOGIN = "https://client.falixnodes.net/auth/login"
 TAGET = f"https://client.falixnodes.net/timer?id={NUM}"
 # ===========================================
 
@@ -110,46 +107,14 @@ class FalixNodesRenewal:
                 except:
                     self.log("⚠️ IP 检测跳过...")
 
-                # 2. 登录
-                self.log("🔗 访问登录页面并尝试登录...")
-                sb.uc_open_with_reconnect(LOGIN, reconnect_time=25)
-                time.sleep(5)
-                sb.execute_script("window.scrollBy(0,1000);")
-                self.log("⏳ 开始登录验证Cloudflare")
-                cf_indicators = [
-                    "verify you are human",
-                    "确认您是真人",
-                    "troubleshoot",
-                    "just a moment"
-                ]
-                for i in range(10): # 尝试10次
-                    sb.uc_gui_click_captcha()
-                    time.sleep(3)
-                    page_lower = sb.get_page_source().lower()
-                    if any(x in page_lower for x in cf_indicators):
-                        sb.uc_gui_handle_captcha()
-                        time.sleep(3)
-                        page_lower = sb.get_page_source().lower()
-                    if not any(x in page_lower for x in cf_indicators):
-                        self.log("✅Cloudflare验证已通过")
-                        break
-                sb.type("#email-address", EMAIL)
-                sb.type("#password", PASSWORD)
-                sb.click("button.auth-submit-btn")
-                time.sleep(5)
-
-                login_screenshot = f"{self.screenshot_dir}/login.png"
-                sb.save_screenshot(login_screenshot)
-                self.send_telegram_notify("✅邮箱密码登录完毕", login_screenshot)
-
-                # 3. 访问目标页面
+                # 2. 访问目标页面
                 self.log("🔗 访问目标页面...")
                 sb.uc_open_with_reconnect(TAGET, reconnect_time=25)
                 time.sleep(5)
 
                 before = sb.get_text("#timer-page-countdown") # Renew前剩余时间
 
-                # 4. 验证Cloudflare
+                # 3. 验证Cloudflare
                 self.log("⏳ 开始验证Cloudflare")
                 cf_indicators = [
                     "verify you are human",
@@ -173,7 +138,7 @@ class FalixNodesRenewal:
                 sb.save_screenshot(taget_screenshot)
                 self.send_telegram_notify("✅访问保活页面并通过Cloudflare认证", taget_screenshot)
                 
-                # 5. 点击各种按钮
+                # 4. 点击各种按钮
                 self.log("🖱️ 开始处理点击各种按钮")
 
                 self.log("🖱️ 点击添加时间/Addtime")
@@ -194,7 +159,7 @@ class FalixNodesRenewal:
                 sb.save_screenshot(click_screenshot)
                 self.send_telegram_notify("✅访问页面并通过Cloudflare认证", click_screenshot)
 
-                # 6. 再次访问目标页面
+                # 5. 再次访问目标页面
                 self.log("🔗 再次访问目标页面...")
                 sb.uc_open_with_reconnect(TAGET, reconnect_time=25)
                 time.sleep(5)
@@ -203,7 +168,7 @@ class FalixNodesRenewal:
                 self.log("✅ 全部流程执行完毕")
                 finish_screenshot = f"{self.screenshot_dir}/finish.png"
                 sb.save_screenshot(finish_screenshot)
-                self.send_telegram_notify(f"🎉 FalixNodes 保活成功\n🖥️ 编号: {SERVERNUM}\n🕒 保活前剩余运行时间: {before}\n🚀 保活后剩余运行时间: {after}", finish_screenshot)
+                self.send_telegram_notify(f"🎉 FalixNodes 保活成功\n🖥️ 编号: {NUM}\n🕒 保活前剩余运行时间: {before}\n🚀 保活后剩余运行时间: {after}", finish_screenshot)
             
             except Exception as e:
                 self.log(f"❌ 运行异常: {e}")
